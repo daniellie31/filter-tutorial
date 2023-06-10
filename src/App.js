2ba4af9efff96c9a742df18c9d8977476f97db05
daniellie31 <danielll3112@gmail.com>
2023-06-10 14:46:17 +0700

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Movie from "./Movie";
+import Filter from "./Filter";
+import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [popular, setPopular] = useState([]);
+	const [filtered, setFiltered] = useState([]);
+	const [activeGenre, setActiveGenre] = useState(0);
+
+	useEffect(() => {
+		fetchMovies();
+	}, []);
+
+	const fetchMovies = async () => {
+		const options = {
+			method: "GET",
+			headers: {
+				accept: "application/json",
+				Authorization:
+					"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGNjOWY0YjAxNTdjNzFlYWFhZTNjN2RmMDY0NmFlMSIsInN1YiI6IjY0ODQxN2IzYzlkYmY5MDEzYTA2NjM3NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.baLgTvsa3hBzETOTz2q-EQ0k59w0JcO7Me2m7SITXxE",
+			},
+		};
+
+		const data = await fetch(
+			"https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+			options
+		);
+		const movies = await data.json();
+		console.log(movies);
+
+		setPopular(movies.results);
+		setFiltered(movies.results);
+	};
+
+	return (
+		<div className="App">
+			<Filter
+				popular={popular}
+				setFiltered={setFiltered}
+				activeGenre={activeGenre}
+				setActiveGenre={setActiveGenre}
+			/>
+			<motion.div layout className="popular-movies">
+				<AnimatePresence>
+					{filtered.map((item) => (
+						<Movie key={item.id} movie={item} />
+					))}
+				</AnimatePresence>
+			</motion.div>
+		</div>
+	);
 }
 
 export default App;
